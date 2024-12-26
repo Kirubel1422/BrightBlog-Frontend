dd515d55a489a12bc282e5733f4af2a97a17bf4d
kirubel <kirubel739@gmail.com>
2024-12-26 21:21:12 +0400

    feat: routing for new pages

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,8 @@ import { Container } from "react-bootstrap";
 import UserNavBar from "./components/UserNavBar";
 import News from "./pages/News";
 import Blogs from "./pages/Blogs";
+import NewsDetail from "./pages/NewsDetail";
+import BlogDetail from "./pages/BlogDetail";
 
 const App = () => {
   return (
@@ -13,7 +15,9 @@ const App = () => {
       <Routes>
         <Route element={<Home />} path="/" />
         <Route element={<News />} path="/news" />
+        <Route element={<NewsDetail />} path="/news/:newsId" />
         <Route element={<Blogs />} path="/blogs" />
+        <Route element={<BlogDetail />} path="/blogs/:blogId" />
       </Routes>
     </Container>
   );
