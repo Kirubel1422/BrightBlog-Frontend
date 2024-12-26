3cb3138a7ad5841d65fd07bb769cce62288c09ac
kirubel <kirubel739@gmail.com>
2024-12-26 21:29:26 +0400

    feat: dynamic routing for user nav

--- src/utils/basic.js
+++ src/utils/basic.js
@@ -3,3 +3,11 @@ export const truncateDesc = (text, limit = 100) => {
     ? text.split("").splice(0, limit).join("") + "..."
     : text;
 };
+
+export const routing = (currentPath) => {
+  const path = currentPath ? currentPath.split("/")[1] : [];
+
+  if (!path.length) return "home";
+
+  return path;
+};
